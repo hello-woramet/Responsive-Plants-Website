906145a8cf997161a931438664327629b1f29f55
hello_woramet <helloworamet@gmail.com>
2021-12-05 15:37:25 +0700

    add border of header when scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,10 @@ function linkAction(){
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
